d525e1b6eb302cd8e4e3c9d2faaab45d5a0d227b
Bryan Quek <bryanquek00@gmail.com>
2023-10-02 03:01:10 +1000

    fixed git author bugs

--- app/api/test/route.js
+++ app/api/test/route.js
@@ -7,6 +7,7 @@ import OpenAI from 'openai';
 
 export const dynamic = 'force-dynamic'
 
+
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API, // defaults to process.env["OPENAI_API_KEY"]
 });
