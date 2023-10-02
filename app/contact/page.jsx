ced0caca39b102b6aea11fc786460b908f6cdaf8
Bryan Quek <bryanquek00@gmail.com>
2023-10-02 11:41:07 +1000

    Changed contact message

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -28,7 +28,7 @@ export default async function Home() {
         title="Contact" 
         left={true}
         image="me.jpeg"
-        text="I am a final year computer science student at the University of Queensland and I am passionate about Full Stack Development and creating web applications. This is a Japanese learning app I created for my Japanese elective class I'm doing in university. I also really love Japanese culture and hope to work there one day"
+        text="I'm in my final year studying computer science at the University of Queensland, and I have a deep interest in Full Stack Development and web application creation. I developed this automated assignment marking system for a hackathon. Should you come across any issues or have suggestions for enhancement, please feel free to reach out!"
         >
           <ContactLink/>
         </BasicCard>
